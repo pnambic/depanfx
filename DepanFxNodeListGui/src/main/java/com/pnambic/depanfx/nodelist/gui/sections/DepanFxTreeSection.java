fb9d97e15508aaa06f774d60e41f661faaa1e32b
Lee Carver <leeca@pnambic.com>
2025-01-08 15:45:47 -0600

    Use node simple name for tree sections

--- DepanFxNodeListGui/src/main/java/com/pnambic/depanfx/nodelist/gui/sections/DepanFxTreeSection.java
+++ DepanFxNodeListGui/src/main/java/com/pnambic/depanfx/nodelist/gui/sections/DepanFxTreeSection.java
@@ -103,14 +103,7 @@ public class DepanFxTreeSection implements DepanFxNodeListSection {
     // Node registry may need to parse node to deliver best display name.
     // For example, Java method name would be better than full signature.
     // This works for current simple paths from FileSystem objects.
-    String nodeKey = node.getId().getNodeKey();
-    try {
-      Path nodePath = Path.of(nodeKey);
-      String result = nodePath.getFileName().toString();
-      return result;
-    } catch (Exception any) {
-    }
-    return nodeKey;
+    return node.getId().getSimpleName();
   }
 
   @Override
