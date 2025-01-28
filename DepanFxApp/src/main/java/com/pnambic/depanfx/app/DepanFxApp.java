98009a421f7d9c62fca415a998b70e5a19197a3f
Lee Carver <leeca@pnambic.com>
2025-01-28 10:04:53 -0600

    Move Spring dependency inject out of init()

--- DepanFxApp/src/main/java/com/pnambic/depanfx/app/DepanFxApp.java
+++ DepanFxApp/src/main/java/com/pnambic/depanfx/app/DepanFxApp.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 The Depan Project Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.pnambic.depanfx.app;
 
 import com.pnambic.depanfx.DepanFxApplication;
@@ -8,41 +23,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import java.io.Closeable;
-import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.stage.Stage;
 
-public class DepanFxApp extends Application implements Closeable {
-
-  private DepanFxSession session;
+public class DepanFxApp extends Application {
 
   private ConfigurableApplicationContext applicationContext;
 
-  @Override
-  public void init() {
-    main();
-  }
+  private DepanFxSession session;
 
   @Override
-  public void start(Stage stage) throws Exception {
-    Platform.setImplicitExit(true);
-
-    session.startSession(stage);
-  }
-
-  @Override
-  public void stop() {
-    session.stopSession();
-    applicationContext.close();
-    Platform.exit();
-  }
-
-  @Override
-  public void close() throws IOException {
-    stop();
+  public void init() throws Exception {
+    // Explicitly just the inherited behaviors.
+    // Initializing Spring during init() leads to problems on MacOS Java
+    // like not being able to use String types in FX dialogs:
+    //
+    // Error messages like:
+    // Caused by: java.lang.NullPointerException: Cannot invoke
+    // "java.lang.ClassLoader.loadClass(String)" because the return value of
+    // "javafx.fxml.FXMLLoader.getClassLoader()" is null.
+    //
+    // As recommended, don't initialize Spring in init().
+    super.init();
   }
 
   /**
@@ -55,14 +58,17 @@ public class DepanFxApp extends Application implements Closeable {
    * See {@link SpringApplication#deduceMainApplicationClass()} for details
    * regarding the discovery of the main application class.
    */
-  public void main() {
-    String[] args = getParameters().getRaw().toArray(new String[0]);
+  @Override
+  public void start(Stage stage) throws Exception {
+    Platform.setImplicitExit(true);
 
+    // Start Spring dependency injection framework (after init()).
+    String[] args = getParameters().getRaw().toArray(new String[0]);
     this.applicationContext = new SpringApplicationBuilder()
-        // .main(getClass())
         .sources(DepanFxApplication.class)
         .run(args);
 
+    // Configure and run the session.
     this.session = applicationContext.getBean(DepanFxSession.class);
     session.setOnClose(applicationContext::close);
 
@@ -71,5 +77,14 @@ public class DepanFxApp extends Application implements Closeable {
     session.setSessionConfig(sessionArgs.getSessionConfig());
     sessionArgs.getSessionPath()
         .ifPresent(session::setSessionPath);
+
+    session.startSession(stage);
+  }
+
+  @Override
+  public void stop() {
+    session.stopSession();
+    applicationContext.close();
+    // Stop is called by Platform.exit();
   }
 }
