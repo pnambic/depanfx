7b988ba1bc9469b47e82956f0be504b9a25a43cf
Lee Carver <leeca@pnambic.com>
2025-11-06 12:49:42 -0600

    Reduce direct app dependencies based on transative module require analysis

--- DepanFxApp/src/main/java/module-info.java
+++ DepanFxApp/src/main/java/module-info.java
@@ -1,7 +1,4 @@
 module depanfx.app {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires transitive javafx.graphics;
     requires net.rgielen.fxweaver.core;
 
     requires spring.boot;
@@ -12,25 +9,17 @@ module depanfx.app {
 
     requires depanfx.scene;
     requires depanfx.session;
-    requires depanfx.session.data;
 
     // For Spring @Component discovery
     requires depanfx.filesystem.gui;
     requires depanfx.filesystem.nodeview;
-    requires depanfx.git;
     requires depanfx.git.gui;
-    requires depanfx.graph_doc;
-    requires depanfx.java;
     requires depanfx.java.gui;
     requires depanfx.java.nodeview;
-    requires depanfx.nodelist.gui;
     requires depanfx.nodelist.viewer;
     requires depanfx.nodeview.gui;
-    requires depanfx.perspective;
     requires depanfx.persistence;
     requires depanfx.workspace.gui;
-    requires depanfx.workspace;
-    requires spring.beans;
 
     opens com.pnambic.depanfx to spring.core;
     opens com.pnambic.depanfx.app to spring.core;
