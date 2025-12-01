d7ada2299513182269e26e9c6447bf484188536e
Lee Carver <leeca@pnambic.com>
2025-12-01 12:35:48 -0800

    Add editor dialog for node list viewer data

--- DepanFxNodeListViewer/src/main/java/com/pnambic/depanfx/nodelist/viewer/DepanFxNodeListViewerConfiguration.java
+++ DepanFxNodeListViewer/src/main/java/com/pnambic/depanfx/nodelist/viewer/DepanFxNodeListViewerConfiguration.java
@@ -26,9 +26,11 @@ import com.pnambic.depanfx.nodelist.gui.DepanFxSaveNodeListDialog;
 import com.pnambic.depanfx.nodelist.gui.columns.DepanFxColumnRegistry;
 import com.pnambic.depanfx.nodelist.model.DepanFxNodeList;
 import com.pnambic.depanfx.nodelist.tooldata.DepanFxNodeListTableViewData;
+import com.pnambic.depanfx.nodelist.viewdata.DepanFxNodeListViewerData;
 import com.pnambic.depanfx.perspective.plugins.DepanFxResourceRegistryContribution;
 import com.pnambic.depanfx.scene.DepanFxDialogRunner;
 import com.pnambic.depanfx.scene.DepanFxSceneService;
+import com.pnambic.depanfx.scene.plugins.DepanFxNewAnalysisContribution;
 import com.pnambic.depanfx.scene.plugins.DepanFxSceneMenuContribution;
 import com.pnambic.depanfx.scene.plugins.DepanFxSceneMenuItems;
 import com.pnambic.depanfx.workspace.DepanFxWorkspace;
@@ -43,6 +45,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.nio.file.Path;
+
+import javafx.scene.control.MenuItem;
 
 @Configuration
 public class DepanFxNodeListViewerConfiguration {
@@ -92,6 +96,23 @@ public class DepanFxNodeListViewerConfiguration {
       DepanFxNodeFiltersRegistry filterRegistry,
       DepanFxNodeFiltersDialogRegistry filterDialogRegistry) {
     return new GraphDocResourceContribution(
+        columnRegistry, infoRegistry, matcherRegistry,
+        filterRegistry, filterDialogRegistry);
+  }
+
+  @Bean
+  public DepanFxNewAnalysisContribution nodeListViewerEditor(
+      DepanFxWorkspace workspace,
+      DepanFxSceneService sceneSrvc,
+      DepanFxDialogRunner dialogRunner,
+      DepanFxColumnRegistry columnRegistry,
+      DepanFxInfoRegistry infoRegistry,
+      DepanFxLinkMatchersRegistry matcherRegistry,
+      DepanFxNodeFiltersRegistry filterRegistry,
+      DepanFxNodeFiltersDialogRegistry filterDialogRegistry) {
+
+    return new NodeListViewerEditorContribution(
+        workspace, sceneSrvc, dialogRunner,
         columnRegistry, infoRegistry, matcherRegistry,
         filterRegistry, filterDialogRegistry);
   }
@@ -279,6 +300,69 @@ public class DepanFxNodeListViewerConfiguration {
           graphRsrc,
           DepanFxNodeListViewBuiltIns.guessTableViewResource(
               workspace, graphRsrc));
+    }
+  }
+
+  private static class NodeListViewerEditorContribution
+      implements DepanFxNewAnalysisContribution {
+
+    private final DepanFxWorkspace workspace;
+
+    private final DepanFxSceneService sceneSrvc;
+
+    private final DepanFxDialogRunner dialogRunner;
+
+    private final DepanFxColumnRegistry columnRegistry;
+
+    private final DepanFxInfoRegistry infoRegistry;
+
+    private final DepanFxLinkMatchersRegistry matcherRegistry;
+
+    private final DepanFxNodeFiltersRegistry filterRegistry;
+
+    private final DepanFxNodeFiltersDialogRegistry filterDialogRegistry;
+
+    public NodeListViewerEditorContribution(
+        DepanFxWorkspace workspace,
+        DepanFxSceneService sceneSrvc,
+        DepanFxDialogRunner dialogRunner,
+        DepanFxColumnRegistry columnRegistry,
+        DepanFxInfoRegistry infoRegistry,
+        DepanFxLinkMatchersRegistry matcherRegistry,
+        DepanFxNodeFiltersRegistry filterRegistry,
+        DepanFxNodeFiltersDialogRegistry filterDialogRegistry) {
+
+      this.workspace = workspace;
+      this.sceneSrvc = sceneSrvc;
+      this.dialogRunner = dialogRunner;
+      this.columnRegistry = columnRegistry;
+      this.infoRegistry = infoRegistry;
+      this.matcherRegistry = matcherRegistry;
+      this.filterRegistry = filterRegistry;
+      this.filterDialogRegistry = filterDialogRegistry;
+    }
+
+    @Override
+    public MenuItem createNewResourceMenuItem() {
+      MenuItem result = new MenuItem(OPEN_LIST_VIEW_LABEL);
+      result.setOnAction(e -> runEditorDialog());
+      return result;
+    }
+
+    private void runEditorDialog() {
+      DepanFxNodeListViewerDataDialog.runEditDialog(workspace, dialogRunner)
+          .ifPresent(this::addViewerFromData);
+    }
+
+    private void addViewerFromData(DepanFxNodeListViewerData viewerData) {
+      DepanFxNodeListViewer viewer = new DepanFxNodeListViewer(
+          viewerData.getViewerTitle(), workspace, dialogRunner,
+          columnRegistry, infoRegistry, matcherRegistry,
+          filterRegistry, filterDialogRegistry);
+
+      sceneSrvc.addViewer(viewer);
+      viewer.initFromNodeListResource(
+          viewerData.getNodeListRsrc(), viewerData.getTableViewRsrc());
     }
   }
 
